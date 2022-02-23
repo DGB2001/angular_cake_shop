a5c36f593339b9c1f4359d3c84939a69004bc2af
Dao Gia Bao <daogiabao2001@gmail.com>
2022-02-23 09:48:42 +0700

    Update components

--- Angular_Cake_Shop/src/app/app.module.ts
+++ Angular_Cake_Shop/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedComponent } from './shared/shared.component';
+import { CoreComponent } from './core/core.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SharedComponent,
+    CoreComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,

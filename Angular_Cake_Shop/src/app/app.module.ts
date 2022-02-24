27f1d320d1813d00f35b293181c9d21c9c4424be
Dao Gia Bao <daogiabao2001@gmail.com>
2022-02-24 17:53:21 +0700

    add headeramd footer components

--- Angular_Cake_Shop/src/app/app.module.ts
+++ Angular_Cake_Shop/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { SharedComponent } from './shared/shared.component';
 import { CoreComponent } from './core/core.component';
 import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { AdminComponent } from './admin/admin.component';
     HomeComponent,
     SharedComponent,
     CoreComponent,
-    AdminComponent
+    AdminComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
